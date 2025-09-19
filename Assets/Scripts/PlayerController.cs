9b9943c24f92b1abc2d15e956ca0ba55f3fad06a
Yayapipi <yayapipi@hotmail.com>
2025-09-19 21:10:39 +0800

    導入角色

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,10 +20,6 @@ public class PlayerController : MonoBehaviour
     private bool isGrounded;
     private bool isFacingRight = true;
 
-    // 【新增】武器與瞄準
-    private BubbleWeapon bubbleWeapon;
-    private Vector2 aimDirection = Vector2.right;
-
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -35,9 +31,6 @@ public class PlayerController : MonoBehaviour
         // 訂閱事件
         playerInputActions.Player.Jump.performed += Jump;
         playerInputActions.Player.Attack.performed += Attack; // 【新增】訂閱攻擊事件
-
-        // 【新增】取得武器元件
-        bubbleWeapon = GetComponent<BubbleWeapon>();
     }
 
     private void OnEnable() { playerInputActions.Player.Enable(); }
@@ -46,19 +39,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         moveInput = playerInputActions.Player.Move.ReadValue<Vector2>();
-
-        // 【新增】以滑鼠位置做 360° 瞄準並同步箭頭
-        UpdateAimFromMouse();
-
-        // 【新增】滑鼠左鍵點擊也可發射泡泡
-        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
-        {
-            if (bubbleWeapon != null && bubbleWeapon.TryFire())
-            {
-                animator.SetTrigger("Attack");
-            }
-        }
-
         UpdateAnimationParameters();
         Flip();
     }
@@ -75,8 +55,7 @@ public class PlayerController : MonoBehaviour
         if (isGrounded) // 假設只有在地面才能攻擊
         {
             animator.SetTrigger("Attack");
-            // 【新增】實際發射泡泡
-            bubbleWeapon?.TryFire();
+            // 在這裡可以加上實際的攻擊邏輯，例如產生子彈或傷害判定
             Debug.Log("Player Attacks!");
         }
     }
@@ -96,35 +75,15 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("IsGround", isGrounded);
     }
 
-    // 【變更】翻面：優先依瞄準方向 X，若接近 0 則退回用移動方向
     private void Flip()
     {
-        float refX = Mathf.Abs(aimDirection.x) >= 0.01f ? aimDirection.x : moveInput.x;
-
-        if ((refX < 0 && isFacingRight) || (refX > 0 && !isFacingRight))
+        if ((moveInput.x < 0 && isFacingRight) || (moveInput.x > 0 && !isFacingRight))
         {
             isFacingRight = !isFacingRight;
             Vector3 theScale = transform.localScale;
             theScale.x *= -1;
             transform.localScale = theScale;
         }
-    }
-
-    // 【新增】以滑鼠位置更新瞄準並同步到武器與箭頭
-    private void UpdateAimFromMouse()
-    {
-        if (Camera.main == null || Mouse.current == null)
-            return;
-
-        Vector2 mouseScreen = Mouse.current.position.ReadValue();
-        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreen.x, mouseScreen.y, 0f));
-
-        Vector3 origin = transform.position;
-        if (bubbleWeapon != null && bubbleWeapon.firePoint != null)
-            origin = bubbleWeapon.firePoint.position;
-
-        aimDirection = ((Vector2)(mouseWorld - origin)).normalized;
-        bubbleWeapon?.SetAim(aimDirection);
     }
 
     private void OnDrawGizmosSelected()
